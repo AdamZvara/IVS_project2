95bc84bdd2de4140eb41698b39dfbe46a174e3ea
Adam Zvara <xzvara01@stud.fit.vutbr.cz>
2021-03-24 11:10:51 +0100

    Add outputs from profiler
    - added images from profiler
    - vystup.pstat is profiler ouput for 1000 numbers tested
    - minor changes to profiling.py

--- src/profiling.py
+++ src/profiling.py
@@ -17,15 +17,14 @@ import mathlib
 # @param num_list List of numbers to calculate standard deviation
 #
 def standard_deviation(num_list):
-    avg = 0
+    avg = result = 0
     n = len(num_list)
     for num in num_list:
+        result = mathlib.add(mathlib.prw(int(num), 2), result)
         avg = mathlib.add(int(num), avg)
     avg = mathlib.div(avg, n)
-    result = 0
-    for num in num_list:
-        result = mathlib.add(mathlib.prw(mathlib.sub(int(num), avg), 2), result)
-    s = mathlib.root(mathlib.mul(result, mathlib.div(1, n - 1)), 2)
+    avg = mathlib.mul(n, mathlib.prw(avg, 2))
+    s = mathlib.root(mathlib.mul(mathlib.sub(result, avg), mathlib.div(1, n - 1)), 2)
     print(s)
 
 
@@ -38,19 +37,18 @@ def generate_numbers(amount):
     f = open("numbers", "w")
     for i in range(1, amount+1):
         random.seed()
-        # maybe add rounding
-        num = str(random.randint(1, amount))
+        # num = str(random.randint(1, amount))
+        num = str(i)
         f.write(num + " ")
     f.close()
 
 
 if __name__ == "__main__":
     numbers = []
-    generate_numbers(1000)
+    # generate_numbers(1000)
     # read redirected stdin
     for line in sys.stdin:
         numbers.extend(line.split())
         if not numbers:
             break
-
     standard_deviation(numbers)
